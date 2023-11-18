21c328cabe4d9e0e13727782fcdd15ac328806ee
tanthana48 <113759642+tanthana48@users.noreply.github.com>
2023-11-18 21:57:53 +0700

    fix notification

--- notification-service/src/database.py
+++ notification-service/src/database.py
@@ -4,7 +4,7 @@ db = SQLAlchemy()
 
 class Notification(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    message = db.Column(db.String, nullable=True)
+    message = db.Column(db.String(255), nullable=True)
     video_id = db.Column(db.Integer, nullable=False)
     user_id = db.Column(db.Integer, nullable=False)
     read = db.Column(db.Boolean , nullable=False, default=False)
